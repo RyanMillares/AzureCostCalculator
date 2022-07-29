259b7eb08c57c8905339850f5df4624f975404ac
Andrey Risukhin <andrey.risukhin@avanade.com>
2022-07-29 09:15:40 -0700

    (new local repo) Committed functional create table

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,6 +1,7 @@
 -- Using Schema in the OneNote
 -- Likely do not need to specify BEGIN TRANSACTION
 -- Good practice to control for modified tables with DROP TABLE IF EXISTS
+-- 7/28/2022: Write tests to check dropping behavior. 
 
 -- Stores number of servers and the app size, rarely modified, mainly read from.
 DROP TABLE IF EXISTS ServerSizes;
@@ -14,7 +15,7 @@ DROP TABLE IF EXISTS IaaS_Web;
 CREATE TABLE IaaS_Web (
 	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
 	cpu INT,
-	ram DECMINAL,
+	ram NUMERIC,
 	storage INT,
 	cost INT
 );
@@ -23,7 +24,7 @@ DROP TABLE IF EXISTS IaaS_API;
 CREATE TABLE IaaS_API (
 	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
 	cpu INT,
-	ram DECMINAL,
+	ram NUMERIC,
 	storage INT,
 	cost INT
 );
@@ -32,7 +33,7 @@ DROP TABLE IF EXISTS IaaS_DB;
 CREATE TABLE IaaS_DB (
 	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
 	cpu INT,
-	ram DECMINAL,
+	ram NUMERIC,
 	storage INT,
 	cost INT
 );
