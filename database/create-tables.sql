eba56b67415f41e37e59e54115e94b520dccc65d
Andrey Risukhin <andrey.risukhin@avanade.com>
2022-07-29 10:13:59 -0700

    Hardcoded tables are correctly imported. Run create-tables.sql, followed by import-tables.sql.

--- database/create-tables.sql
+++ database/create-tables.sql
@@ -1,19 +1,23 @@
 -- Using Schema in the OneNote
 -- Likely do not need to specify BEGIN TRANSACTION
--- Good practice to control for modified tables with DROP TABLE IF EXISTS
--- 7/28/2022: Write tests to check dropping behavior. 
+-- TODO: Write tests to check dropping behavior. 
+-- TODO: before production, optimize sizes of fields; DECIMAL(10,2) vs INT for servers, that kind of thing
+
 
 -- Stores number of servers and the app size, rarely modified, mainly read from.
 DROP TABLE IF EXISTS ServerSizes;
 CREATE TABLE ServerSizes (
-	ssid VARCHAR(3) PRIMARY KEY, -- Primary Key (implicit NOT NULL constraint)
+	ssid UNIQUEIDENTIFIER PRIMARY KEY default NEWID(), -- Using GUIDs as primary keys for security (implicit nonnull)
+	--ssid VARCHAR(3) PRIMARY KEY, 
 	size VARCHAR(6),
-	servers INT
+	servers INT -- TODO check this is not a protected keyword
 );
 
+-- These tables are often read from, sometimes writted to (updated costs via web API)
 DROP TABLE IF EXISTS IaaS_Web;
 CREATE TABLE IaaS_Web (
-	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
+	iwid UNIQUEIDENTIFIER PRIMARY KEY default NEWID(),
+	vm VARCHAR(7), -- Store the vm name D1_v2 notation as the primary key
 	cpu INT,
 	ram NUMERIC,
 	storage INT,
@@ -22,7 +26,8 @@ CREATE TABLE IaaS_Web (
 
 DROP TABLE IF EXISTS IaaS_API;
 CREATE TABLE IaaS_API (
-	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
+	iaid UNIQUEIDENTIFIER PRIMARY KEY default NEWID(),
+	vm VARCHAR(7), 
 	cpu INT,
 	ram NUMERIC,
 	storage INT,
@@ -31,7 +36,8 @@ CREATE TABLE IaaS_API (
 
 DROP TABLE IF EXISTS IaaS_DB;
 CREATE TABLE IaaS_DB (
-	iwid VARCHAR(7) PRIMARY KEY, -- Use the D1_v2 notation as the primary key
+	idid UNIQUEIDENTIFIER PRIMARY KEY default NEWID(),
+	vm VARCHAR(7), 
 	cpu INT,
 	ram NUMERIC,
 	storage INT,
