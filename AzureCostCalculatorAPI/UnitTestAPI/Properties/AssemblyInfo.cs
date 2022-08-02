78926959d30079d2ac25131596b40c9a8f5ed350
Andrey Risukhin <andrey.risukhin@avanade.com>
2022-08-02 16:05:50 -0700

    Added API Unit Test project, needs completion

---
+++ AzureCostCalculatorAPI/UnitTestAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6b05d7c-79bf-44f9-bc83-787adb0c30ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AzureCostCalculatorAPI/UnitTestAPI/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestAPI
+{
+    [TestClass]
+    
+    // RETRIEVAL TESTING
+    // Does the API correctly retrieve data from the database?
+    // Compare expected and actual returned values, given known DB state.
+
+    // Each "unit" is a request and fields in that request, test these
+
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    // TODO put these into other test files
+
+    // MATH TESTS
+    // Does the Frontend correctly perform math checks?
+    // Ensure user is limited to A+B+C=N servers for a server count N
+    /*
+     
+     */
+    
+
+}
