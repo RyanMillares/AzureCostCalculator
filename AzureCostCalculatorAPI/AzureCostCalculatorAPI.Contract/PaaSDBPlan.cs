de690bd1731e99b5229451e19b8011944f55115d
Sarah Zilz <sarah.zilz@avanade.com>
2022-08-04 13:52:11 -0700

    added PaaS controllers and PaaS plan objects

---
+++ AzureCostCalculatorAPI/AzureCostCalculatorAPI.Contract/PaaSDBPlan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureCostCalculatorAPI.Contract
+{
+    public class PaaSDBPlan
+    {
+        public Guid PDID { get; set; }
+
+        public int? Cost { get; set; }
+    }
+}
---
+++ AzureCostCalculatorAPI/AzureCostCalculatorAPI/Controllers/PaaSDBController.cs
@@ -0,0 +1,24 @@
+﻿using AzureCostCalculatorAPI.Contract;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AzureCostCalculatorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaaSDBController : ControllerBase
+    {
+        [HttpGet]
+        // Returns a list of all the IaaS API plans
+        public async Task<List<PaaSDBPlan>> GetPaaSDBPlan()
+        {
+            using IDbConnection conn = new SqlConnection("Server=.;Trusted_Connection=True;Database=AzureResources;TrustServerCertificate=True;");
+            var PaaSDBData = await conn.QueryAsync<PaaSDBPlan>("select * from PaaS_DB");
+            return PaaSDBData.ToList();
+        }
+    }
+}
