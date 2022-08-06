1ac1320a11adc985da46dbe8127a79236ab36359
Jason Khalili <jason.khalili@avanade.com>
2022-08-06 16:31:19 -0700

    Deleted table imports

--- frontend/src/Pricing.js
+++ frontend/src/Pricing.js
@@ -17,12 +17,6 @@ import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 
 
 const theme = createTheme({
