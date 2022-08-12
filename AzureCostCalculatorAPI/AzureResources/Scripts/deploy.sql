7eb2fc7a5f91bf45802b0f170866d0464dbc8790
Andrey Risukhin <andrey.risukhin@avanade.com>
2022-08-12 12:08:20 -0700

    deployment script work, added Scripts/ folder, PaaS tables to be pushed.

---
+++ AzureCostCalculatorAPI/AzureResources/Scripts/deploy.sql
@@ -0,0 +1,10 @@
+﻿/*
+Author: Andrey Risukhin
+Purpose: The script to run when publishing a database, to automatically set up AzureCostCalculator.
+Description: create the tables (schema), and import the initial hardcoded values based on the Excel sheet.
+
+*/
+
+-- https://stackoverflow.com/questions/5237198/transactsql-to-run-another-transactsql-script
+-- :r ./create-tables.sql
+-- :r ./import-tables.sql
