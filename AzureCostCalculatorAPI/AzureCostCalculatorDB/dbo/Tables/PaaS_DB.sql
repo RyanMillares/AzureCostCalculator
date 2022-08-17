af0c7a43f011f698f5f08521d0bd754253494d66
Andrey Risukhin <andrey.risukhin@avanade.com>
2022-08-17 14:34:27 -0700

    New DB project: AzureCostCalculatorDB. Deploy, other scripts live inside folder.

---
+++ AzureCostCalculatorAPI/AzureCostCalculatorDB/dbo/Tables/PaaS_DB.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PaaS_DB] (
+    [pdid]     UNIQUEIDENTIFIER DEFAULT (newid()) NOT NULL,
+    [type]     VARCHAR (32)     NULL,
+    [hardware] VARCHAR (16)     NULL,
+    [storage]  VARCHAR (16)     NULL,
+    [instance] VARCHAR (16)     NULL,
+    [cost]     INT              NULL,
+    PRIMARY KEY CLUSTERED ([pdid] ASC)
+);
+
