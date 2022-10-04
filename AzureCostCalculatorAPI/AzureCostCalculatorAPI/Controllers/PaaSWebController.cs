67f2e2294f5813a2116b35082fbc979b825b5c03
Ryan Millares <ryan.millares@avanade.com>
2022-10-04 09:12:52 -0700

    added entityframework but no realchanges yet

--- AzureCostCalculatorAPI/AzureCostCalculatorAPI/Controllers/PaaSWebController.cs
+++ AzureCostCalculatorAPI/AzureCostCalculatorAPI/Controllers/PaaSWebController.cs
@@ -1,8 +1,11 @@
 ﻿using AzureCostCalculatorAPI.Contract;
+using Microsoft.EntityFrameworkCore;
 using Dapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client.Extensions.Msal;
 using System.Data;
 using System.Data.SqlClient;
+using static System.Data.IDbConnection;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -32,6 +35,43 @@ namespace AzureCostCalculatorAPI.Controllers
             using IDbConnection conn = new SqlConnection(myConnectorString);
             var plan = await conn.QuerySingleAsync<PaaSWebPlan>("select * from PaaS_Web where pwid = @id", new { id });
             return plan;
+        }
+        [HttpPost("paasweb/Post")]
+        public async Task<PaaSWebPlan> Post(String pwid, String name, int cpu, int ram, int storage, int cost)
+        {
+            var myConnectorString = ConfigHandler.GetByName("SqlConnectorString");
+            using IDbConnection conn = new SqlConnection(myConnectorString);
+
+            String sql = $"INSERT INTO PaaSWeb (pwid,name,cpu,ram,storage,cost) VALUES ({pwid},{name}, {cpu}, {ram}, {storage}, {cost})";
+   
+            
+            //return plan;
+
+            
+            String query = "INSERT INTO PaaSWeb (pwid, name,cpu,ram,storage,cost) VALUES (@pwid, @name, @cpu, @ram, @storage, @cost)";
+
+            SqlCommand command = new SqlCommand(query);
+            //command.Connection = conn;
+            command.Parameters.AddWithValue("@pwid", pwid);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@cpu", cpu);
+            command.Parameters.AddWithValue("@ram", ram);
+            command.Parameters.AddWithValue("@storage", storage);
+            command.Parameters.AddWithValue("@cost", cost);
+
+            conn.Open();
+            var result = command.ExecuteNonQuery();
+
+            if (result < 0)
+                Console.WriteLine("Error inserting data!");
+
+                //return result;
+            
+            //Console.WriteLine(sql);
+            var plan = await conn.QuerySingleAsync<PaaSWebPlan>(sql);
+            return plan;
+
+
         }
     }
 }
