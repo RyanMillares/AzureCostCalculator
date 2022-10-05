eed848156808cba072f1d1e75ff25c42edec31fd
Ryan Millares <ryan.millares@avanade.com>
2022-10-05 11:35:00 -0700

    pain and suffering with CORS error in axios post

--- frontend/src/Pricing.js
+++ frontend/src/Pricing.js
@@ -288,14 +288,40 @@ function PricingContent() {
                 **/
 
                 //permanent method
-                const paasWebUrl = "https://" + apiServerName + ":7056/api/PaaSWeb";
 
+                const paasWebUrl = "https://" + apiServerName + ":7056/api/PaaSWeb";
+                console.log(newTier)
+
+
+                /**
+                  // FETCH METHOD
+
+  
+                const requestOptions = {
+                    method: "POST",
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                    },
+                    body: JSON.stringify(newTier)
+                }
+                fetch(paasWebUrl, requestOptions).then(response => response.json())
+                **/
+                
+                // AXIOS METHOD
+                /**
+                const client = axios.create({
+                    baseURL: paasWebUrl,
+                    Headers: {
+                        'Content-Type': 'text/plain;charset=utf-8',
+                    }
+                });
+                **/
                 let res = axios.post(paasWebUrl, newTier
                     
                     ,{
                         headers: {
-                            //"Content-Type": "application/json",
-                            //"Access-Control-Allow-Headers": "Access-Control-Allow-Headers, Authorization, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials"
+                            //"content-type": "text/plain;charset=utf-8"//,
+                            "Access-Control-Allow-Headers": "Accept, Accept-CH, Accept-Charset, Accept-Datetime, Accept-Encoding, Accept-Ext, Accept-Features, Accept-Language, Accept-Params, Accept-Ranges, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Expose-Headers, Access-Control-Max-Age, Access-Control-Request-Headers, Access-Control-Request-Method, Age, Allow, Alternates, Authentication-Info, Authorization, C-Ext, C-Man, C-Opt, C-PEP, C-PEP-Info, CONNECT, Cache-Control, Compliance, Connection, Content-Base, Content-Disposition, Content-Encoding, Content-ID, Content-Language, Content-Length, Content-Location, Content-MD5, Content-Range, Content-Script-Type, Content-Security-Policy, Content-Style-Type, Content-Transfer-Encoding, Content-Type, Content-Version, Cookie, Cost, DAV, DELETE, DNT, DPR, Date, Default-Style, Delta-Base, Depth, Derived-From, Destination, Differential-ID, Digest, ETag, Expect, Expires, Ext, From, GET, GetProfile, HEAD, HTTP-date, Host, IM, If, If-Match, If-Modified-Since, If-None-Match, If-Range, If-Unmodified-Since, Keep-Alive, Label, Last-Event-ID, Last-Modified, Link, Location, Lock-Token, MIME-Version, Man, Max-Forwards, Media-Range, Message-ID, Meter, Negotiate, Non-Compliance, OPTION, OPTIONS, OWS, Opt, Optional, Ordering-Type, Origin, Overwrite, P3P, PEP, PICS-Label, POST, PUT, Pep-Info, Permanent, Position, Pragma, ProfileObject, Protocol, Protocol-Query, Protocol-Request, Proxy-Authenticate, Proxy-Authentication-Info, Proxy-Authorization, Proxy-Features, Proxy-Instruction, Public, RWS, Range, Referer, Refresh, Resolution-Hint, Resolver-Location, Retry-After, Safe, Sec-Websocket-Extensions, Sec-Websocket-Key, Sec-Websocket-Origin, Sec-Websocket-Protocol, Sec-Websocket-Version, Security-Scheme, Server, Set-Cookie, Set-Cookie2, SetProfile, SoapAction, Status, Status-URI, Strict-Transport-Security, SubOK, Subst, Surrogate-Capability, Surrogate-Control, TCN, TE, TRACE, Timeout, Title, Trailer, Transfer-Encoding, UA-Color, UA-Media, UA-Pixels, UA-Resolution, UA-Windowpixels, URI, Upgrade, User-Agent, Variant-Vary, Vary, Version, Via, Viewport-Width, WWW-Authenticate, Want-Digest, Warning, Width, X-Content-Duration, X-Content-Security-Policy, X-Content-Type-Options, X-CustomHeader, X-DNSPrefetch-Control, X-Forwarded-For, X-Forwarded-Port, X-Forwarded-Proto, X-Frame-Options, X-Modified, X-OTHER, X-PING, X-PINGOTHER, X-Powered-By, X-Requested-With"
 
                             //"Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
                             //"Access-Control-Allow-Origin": "localhost",
@@ -303,10 +329,11 @@ function PricingContent() {
                             //"Access-Control-Allow-Credentials": true,
                         }
                     }
-                        
-                )
-                console.log(res);
-                console.log(res.data)
+                    
+                       
+                ).then((response) => console.log(response.data))
+              
+                
                 break;
 
 
